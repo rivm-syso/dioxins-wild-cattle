50665afc427179983f0435e7f02396ab35fec583
minnemj <jordi.minnema@rivm.nl>
2024-11-20 08:53:18 +0100

    Change order of installation to avoid warnigs

--- setup.R
+++ setup.R
@@ -1,15 +1,15 @@
 #' File to install all needed packages to run the scripts in this repository
+install.packages("lifecycle", force=TRUE)
 install.packages("magrittr", force=FALSE)
 install.packages("dplyr", force=FALSE)
 install.packages("deSolve", force=FALSE)
 install.packages("devtools", dependencies=T, force=FALSE)
 install.packages("tidyverse", force=FALSE)
 install.packages("reticulate", force=FALSE)
-install.packages("lifecycle", force=TRUE)
 install.packages("ggplot2", force=FALSE)
 
 # Installation of (mini-)conda and creating a virtual environment. Only run this if not already done
-reticulate::install_miniconda()
+reticulate::install_miniconda(force=FALSE)
 reticulate::conda_create("nested_sampling")
 
 # Install python packages through reticulate and miniconda
