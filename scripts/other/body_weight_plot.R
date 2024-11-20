041973cbbbf69432f51088b97d051b1cf6ee7066
minnemj <jordi.minnema@rivm.nl>
2024-11-20 09:09:28 +0100

    Cleaning of file

--- scripts/other/body_weight_plot.R
+++ scripts/other/body_weight_plot.R
@@ -1,6 +1,8 @@
 #' This script is used to visualize the body weight over time, as well as the 
 #' weight of the compartments. 
 
+
+# Load packages
 library(ggplot2)
 library(magrittr)
 
@@ -9,19 +11,18 @@ library(reshape2)
 library(reshape)
 
 library(gridExtra)
+source('functions/helper_functions.R')
 
-# Clear workspace
-rm(list = ls())
 
-# Cow model
+
+# Cows
 source("model/model.R")
 
 parametersCow <- assign_parameters()
-parametersCow <- utilities::param_update(parametersCow, 
-                                         gender = 'cow',
-                                         TSTOP = 14*365,
-                                         relFatVariation = 0
-                                      )
+parametersCow <- param_update(parametersCow, 
+                              gender = 'cow',
+                              TSTOP = 14*365,
+                              relFatVariation = 0)
 solutionCow   <- as.data.frame(run_model(parametersCow))
 solutionCow <- solutionCow[,c("time", "vFat", "vLiver", "vRich", "vSlow", "vBlood", "wgi")]
 colnames(solutionCow)[2] <- "vAdipose"
@@ -29,7 +30,7 @@ solutionCow <- melt(solutionCow, id.vars="time")
 
 
 cowWeights <- data.frame(weight = c(140, 200, 240, 400, 540, 430, 620, 720, 825),
-                  age    = c(0.25, 0.5, 0.75, 1.5, 2, 2.5, 3, 5, 5))
+                         age    = c(0.25, 0.5, 0.75, 1.5, 2, 2.5, 3, 5, 5))
 
 plotComposition1 <- ggplot() + 
   geom_area(aes(x=time/365, y=value, fill=variable), data=solutionCow) +
@@ -39,19 +40,16 @@ plotComposition1 <- ggplot() +
   ylab('Volume (L)') + 
   xlab("Time (years)")
 
-# Bull model
+# Bulls
 parametersBull <- assign_parameters()
-parametersBull <- calculate_parameters(parametersBull) 
-parametersBull <- utilities::param_update(parametersBull, 
-                                      gender = 'bull',
-                                      TSTOP = 14*365,
-                                      relFatVariation = 0)
+parametersBull <- param_update(parametersBull, 
+                               gender = 'bull',
+                               TSTOP = 14*365,
+                               relFatVariation = 0)
 solutionBull   <- as.data.frame(run_model(parametersBull))
 solutionBull <- solutionBull[,c("time",  "vFat", "vLiver", "vRich", "vSlow", "vBlood", "wgi")]
 colnames(solutionBull)[2] <- "vAdipose"
 solutionBull <- melt(solutionBull, id.vars="time")
-
-
 
 bullWeights <- data.frame(weight = c(140, 200, 300, 450, 720, 800, 1000, 1250),
                           age    = c(0.25, 0.5, 0.75, 1.5, 2, 3, 4, 6))
